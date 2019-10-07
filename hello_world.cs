367199ece89c4112b6cc16652ccc07b69bbe8863
freder2002 <43769548+freder2002@users.noreply.github.com>
2019-10-07 11:02:26 -0400

    Create hello_world.cs
    Added your hello world in c#

---
+++ hello_world.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World !\n");
+        }
+    }
+}
